10973b923bfa287d95153d419c080957c8fb2de9
KomalPatil28 <157786779+KomalPatil28@users.noreply.github.com>
2024-02-03 22:33:54 +0530

    Delete Assignment6_4.java

--- Assignment6_4.java
+++
@@ -1,9 +0,0 @@
-/*Create a hierarchy of library items using hierarchical inheritance.Define a class 
-"LibraryItem" with attributes like title and item ID.Derive two classes "Book" and "DVD"
-from the base class.Implement methods to display information about each type of library
-item*/
-package chapter6;
-
-public class Assignment6_4 {
-
-}
